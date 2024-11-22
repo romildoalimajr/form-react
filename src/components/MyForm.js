0c960546b23ce86fbaf4ec1b617a11f6b5b82b87
Romildo A. Lima Jr. <odlimor.sevla@gmail.com>
2024-11-21 22:19:33 -0300

    Gerenciamento de dados no input usando o useState.

--- src/components/MyForm.js
+++ src/components/MyForm.js
@@ -1,26 +1,36 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './MyForm.css';
 
 function MyForm() {
-  return (
-    <div>
-        {/** 1 criação de form */}
-        <form>
-            <div>
-                <label htmlFor="name">Nome.:</label>
-                <input type="text" name='name' placeholder='Digite o seu nome' />
-            </div>
-            {/** 2 label envolvendo o input */}
-            <label htmlFor="">
-                <span>Email.?: </span>
-                <input type="email" name="email" id="email" placeholder='Digite o seu e-mail' />
-            </label>
-            <div>
-                <input type="submit" value="Enviar" />
-            </div>
-        </form>
-    </div>
-  )
+    // 3 gerenciamento de dados
+    const [name, setName] = useState();
+    const [email, setEmail] = useState();
+
+    const handleName = (e) => {
+        setName(e.target.value);
+    };
+
+    console.log(name);
+
+    return (
+        <div>
+            {/** 1 criação de form */}
+            <form>
+                <div>
+                    <label htmlFor="name">Nome.:</label>
+                    <input type="text" name='name' placeholder='Digite o seu nome' onChange={handleName} />
+                </div>
+                {/** 2 label envolvendo o input */}
+                <label htmlFor="">
+                    <span>Email.?: </span>
+                    <input type="email" name="email" id="email" placeholder='Digite o seu e-mail' />
+                </label>
+                <div>
+                    <input type="submit" value="Enviar" />
+                </div>
+            </form>
+        </div>
+    )
 }
 
 export default MyForm
