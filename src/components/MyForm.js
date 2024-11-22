757014f11ea8c38d998208e2aa5750dd1d9daf08
Romildo A. Lima Jr. <odlimor.sevla@gmail.com>
2024-11-21 22:07:07 -0300

    Utilizando Label para envolver o input.

--- src/components/MyForm.js
+++ src/components/MyForm.js
@@ -10,6 +10,11 @@ function MyForm() {
                 <label htmlFor="name">Nome.:</label>
                 <input type="text" name='name' placeholder='Digite o seu nome' />
             </div>
+            {/** 2 label envolvendo o input */}
+            <label htmlFor="">
+                <span>Email.?: </span>
+                <input type="email" name="email" id="email" placeholder='Digite o seu e-mail' />
+            </label>
             <div>
                 <input type="submit" value="Enviar" />
             </div>
